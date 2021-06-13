75983a5806b6fab144fc7ab6cfa5a696b7cc2427
BuyWell <>
2021-06-14 02:43:36 +0300

    connect game and client

---
+++ game.cpp
@@ -0,0 +1,418 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#pragma comment(lib, "ws2_32.lib")
+#include <WinSock2.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <SDL.h>
+#include <SDL_image.h>
+
+SOCKET soc;
+int player_num = 0;
+
+#define SCREEN_WIDTH 1280
+#define SCREEN_HEIGHT 720
+#define GRAVITY 0.35f
+
+
+typedef struct
+{
+	float x;
+	float y;
+	float dx, dy;
+	short life;
+	char* name;
+	int onLedge;
+}Player;
+
+typedef struct
+{
+	int x;
+	int y;
+}Bear;
+
+typedef struct
+{
+	int x, y, w, h;
+}Ledge;
+
+typedef struct
+{
+	Player player[2];
+
+	Bear bears[100];
+
+	Ledge ledges[100];
+
+	SDL_Texture* bear;
+	SDL_Texture* playerFrames[5];
+	SDL_Texture* floor;
+
+	SDL_Renderer* renderer;
+}GameState;
+
+GameState game;
+SDL_Window* window;
+SDL_Renderer* renderer;
+
+void ClientHandler() {
+	char msg[256];
+	while (1) {
+		recv(soc, msg, sizeof(msg), NULL);
+		game.player[player_num].x = atoi(msg);
+	}
+}
+
+void LoadGame(GameState *game)
+{
+	SDL_Surface* surface = NULL;
+
+	//load images and create rendering textures from them
+	surface = IMG_Load("player.png");
+	if (surface == NULL)
+	{
+		printf("Cannot find player.png!\n\n");
+		SDL_Quit();
+		exit(1);
+	}
+
+	game->bear = SDL_CreateTextureFromSurface(game->renderer, surface);
+	SDL_FreeSurface(surface);
+
+	surface = IMG_Load("idle.png");
+	if (surface == NULL)
+	{
+		printf("Cannot find idle.png!\n\n");
+		SDL_Quit();
+		exit(1);
+	}
+
+	game->playerFrames[0] = SDL_CreateTextureFromSurface(game->renderer, surface);
+	SDL_FreeSurface(surface);
+
+	surface = IMG_Load("going1.png");
+	if (surface == NULL)
+	{
+		printf("Cannot find going1.png!\n\n");
+		SDL_Quit();
+		exit(1);
+	}
+
+	game->playerFrames[1] = SDL_CreateTextureFromSurface(game->renderer, surface);
+	SDL_FreeSurface(surface);
+
+	surface = IMG_Load("going2.png");
+	if (surface == NULL)
+	{
+		printf("Cannot find going2.png!\n\n");
+		SDL_Quit();
+		exit(1);
+	}
+
+	game->playerFrames[2] = SDL_CreateTextureFromSurface(game->renderer, surface);
+	SDL_FreeSurface(surface);
+
+	surface = IMG_Load("going3.png");
+	if (surface == NULL)
+	{
+		printf("Cannot find going3.png!\n\n");
+		SDL_Quit();
+		exit(1);
+	}
+
+	game->playerFrames[3] = SDL_CreateTextureFromSurface(game->renderer, surface);
+	SDL_FreeSurface(surface);
+
+	surface = IMG_Load("going4.png");
+	if (surface == NULL)
+	{
+		printf("Cannot find going4.png!\n\n");
+		SDL_Quit();
+		exit(1);
+	}
+
+	game->playerFrames[4] = SDL_CreateTextureFromSurface(game->renderer, surface);
+	SDL_FreeSurface(surface);
+
+	surface = IMG_Load("floor.png");
+	game->floor = SDL_CreateTextureFromSurface(game->renderer, surface);
+	SDL_FreeSurface(surface);
+
+	game->player[player_num].x = 220;
+	game->player[player_num].y = 140;
+	game->player[player_num].dy = 0;
+	game->player[player_num].dx = 0;
+	game->player[player_num].onLedge = 0;
+
+	for (int i = 0; i < 100; ++i)
+	{
+		game->bears[i].x = i * 64;
+		game->bears[i].y = i * 32;
+	}
+
+	//init ledges
+	for (int i = 0; i < 100; i++)
+	{
+		game->ledges[i].w = 256;
+		game->ledges[i].h = 64;
+		game->ledges[i].x = i*256;
+		game->ledges[i].y = 400;
+	}
+	game->ledges[99].x = 350;
+	game->ledges[99].y = 200;
+}
+
+int ProcessEvents(SDL_Window* window, GameState *game)
+{
+	SDL_Event event;
+	int done = 0;
+
+	while (SDL_PollEvent(&event))
+	{
+		switch (event.type)
+		{
+		case SDL_WINDOWEVENT_CLOSE:
+		{
+			if (window)
+			{
+				SDL_DestroyWindow(window);
+				window = NULL;
+				done = 1;
+			}
+		}
+		break;
+		case SDL_KEYDOWN:
+		{
+			switch (event.key.keysym.sym)
+			{
+			case SDLK_ESCAPE:
+				done = 1;
+				break;
+			case SDLK_UP:
+				if (game->player[player_num].onLedge)
+				{
+					game->player[player_num].dy = -15;
+					game->player[player_num].onLedge = 0;
+				}
+				break;
+			}
+		}
+		break;
+		case SDL_QUIT:
+			done = 1;
+			break;
+		}
+	}
+
+	const Uint8 *state = SDL_GetKeyboardState(NULL);
+	if (state[SDL_SCANCODE_LEFT])
+	{
+		game->player[player_num].dx -= 3;
+		if (game->player[player_num].dx < -10)
+		{
+			game->player[player_num].dx = -10;
+		}
+	}
+	if (state[SDL_SCANCODE_RIGHT])
+	{
+		game->player[player_num].dx += 3;
+		if (game->player[player_num].dx > 10)
+		{
+			game->player[player_num].dx = 10;
+		}
+	}
+	else
+	{
+		game->player[player_num].dx *= 0.8f;
+		if (fabsf(game->player[player_num].dx) < 0.1f)
+		{
+			game->player[player_num].dx = 0;
+		}
+	}
+
+	return done;
+}
+
+void DoRender(SDL_Renderer *renderer, GameState* game)
+{
+	//set the drawing color to blue
+	SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+
+	//clear the screen (to blue)
+	SDL_RenderClear(renderer);
+
+	//set the drawing color to white
+	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+
+	for (int i = 0; i < 100; i++)
+	{
+		SDL_Rect ledgeRect = { game->ledges[i].x, game->ledges[i].y, game->ledges[i].w, game->ledges[i].h };
+		SDL_RenderCopy(renderer, game->floor, NULL, &ledgeRect);
+	}
+
+	SDL_Rect rect = { game->player[player_num].x, game->player[player_num].y, 100, 100 };
+	CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)ClientHandler, NULL, NULL, NULL);
+	char msg[256];
+	send(soc, msg, sizeof(msg), NULL);
+	SDL_RenderCopy(renderer, game->playerFrames[0], NULL, &rect, 0, NULL, 0);
+	/*SDL_RenderFillRect(renderer, &rect);*/
+
+	//for (int i = 0; i < 100; i++)
+	//{
+	//	SDL_Rect bearRect = { game->bears[i].x, game->bears[i].y, 64, 64 };
+	//	SDL_RenderCopy(renderer, game->bear, NULL, &bearRect);
+	//}
+
+
+
+	//We are done draing, 'present' or wshow to the screen what we've drawn
+	SDL_RenderPresent(renderer);
+
+}
+
+void process(GameState* game)
+{
+	
+	game->player[player_num].x += game->player[player_num].dx;
+	game->player[player_num].y += game->player[player_num].dy;
+
+	game->player[player_num].dy += GRAVITY;
+}
+
+void CollisionDetect(GameState* game)
+{
+	//Check for collision with any ledges
+	for (int i = 0; i < 100; i++)
+	{
+		float pw = 80;
+		float ph = 90;
+		float px = game->player[player_num].x;
+		float py = game->player[player_num].y;
+		float fx = game->ledges[i].x;
+		float fy = game->ledges[i].y;
+		float fw = game->ledges[i].w;
+		float fh = game->ledges[i].h;
+
+		if (px + pw / 2 > fx && px + pw / 2 < px + pw)
+		{
+			//are we bumpibg our head?
+			if (py < fy + fh && py > fy && game->player[player_num].dy < 0)
+			{
+				//correct y
+				game->player[player_num].y = fy + fh;
+				py = fy + ph;
+
+				//bumped our head, stop any jump velocity
+				game->player[player_num].dy = 0;
+				game->player[player_num].onLedge = 1;
+			}
+		}
+
+		if (px + pw > fx && px < fx + fw)
+		{
+			//we are landing on the ledge
+			if (py + ph > fy && py < fy && game->player[player_num].dy > 0)
+			{
+				//correct y
+				game->player[player_num].y = fy - ph;
+				py = fy - ph;
+
+				//landed on this ledge, stop any jump velocity
+				game->player[player_num].dy = 0;
+				game->player[player_num].onLedge = 1;
+			}
+		}
+
+		if (py + ph > fy && py < fy + fh)
+		{
+			//rubbing agaisnt right edge
+			if (px < fx + fw && px + pw > fx + fw && game->player[player_num].dx < 0)
+			{
+				//correct x
+				game->player[player_num].x = fx + fw;
+				px = fx + fw;
+
+				game->player[player_num].dx = 0;
+			}
+			//rubbing against left edge
+			else if (px + pw > fx && px < fx && game->player[player_num].dx > 0)
+			{
+				//correct x
+				game->player[player_num].x = fx - pw;
+				px = fx - pw;
+
+				game->player[player_num].dx = 0;
+			}
+		}
+	}
+}
+
+#undef main
+int main(int argc, char* args[])
+{
+	WSADATA ws;
+	WSAStartup(MAKEWORD(2, 2), &ws);
+
+	soc = socket(AF_INET, SOCK_STREAM, 0);
+
+	SOCKADDR_IN sa;
+	memset(&sa, 0, sizeof(sa));
+	sa.sin_family = AF_INET;
+	sa.sin_port = htons(8800);
+
+	sa.sin_addr.S_un.S_addr = inet_addr("192.168.3.10");
+
+	if (connect(soc, (const SOCKADDR*)&sa, sizeof(sa)) != 0) {
+		printf("error to connect");
+	}
+	else {
+		printf("connected");
+	}
+	char buf[100] = { 0 };
+	recv(soc, buf, sizeof(buf), NULL);
+	player_num = atoi(buf);
+	printf("Are u number %d\n", player_num);
+
+	SDL_Init(SDL_INIT_EVERYTHING);
+
+	window = SDL_CreateWindow("TheBestGame",
+								SDL_WINDOWPOS_UNDEFINED, // x
+								SDL_WINDOWPOS_UNDEFINED, // y
+								SCREEN_WIDTH, //ширина
+								SCREEN_HEIGHT, //высота
+								SDL_WINDOW_SHOWN); //Задаём окно
+
+	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+	game.renderer = renderer;
+
+	LoadGame(&game);
+
+	int done = 0;
+
+	while (!done)
+	{
+		//Check for events
+		done = ProcessEvents(window, &game);
+
+		process(&game);
+		CollisionDetect(&game);
+
+		//render display
+		DoRender(renderer, &game);
+
+		/*SDL_Delay(10);*/
+	}
+
+	ProcessEvents(window, renderer, done);
+
+	SDL_DestroyTexture(game.bear);
+
+	SDL_DestroyWindow(window);
+	SDL_DestroyRenderer(renderer);
+
+	SDL_Quit();
+
+	return 0;
+}
